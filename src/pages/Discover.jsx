0bc7fc7e02c0baa9e6edf2ee79e2c3d3e1d9ce49
shubham-sanvariya <shubhamsanvariyait.com>
2022-12-10 22:07:07 +0530

    adding selector and dispatch

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,8 +1,13 @@
 import { Error, Loader, SongCard } from "../components";
 import { genres } from '../assets/constants';
 import { useGetTopChartsQuery  } from "../redux/services/shazamCore";
+import { useDispatch, useSelector } from "react-redux";
 
 const Discover = () => {
+    const dispatch = useDispatch();
+    const { activeSong, isPlaying } = useSelector((state) => 
+    state.player);
+
     const { data, isFetching, error } = useGetTopChartsQuery();
     const genreTitle = 'pop';
 
@@ -16,7 +21,7 @@ const Discover = () => {
             <div className="w-full flex justify-between items-center
             sm:flex-row flex-col mt-4 mb-10">
                 <h2 className="font-bold text-3xl text-white
-                text-left">Discover {genreTitle}</h2>
+                text-left"></h2>
                 <select onChange={() => {}}
                 value=""
                 className="bg-black text-gray-300 p-3
@@ -34,6 +39,9 @@ const Discover = () => {
                         <SongCard 
                         key={song.key}
                         song={song}
+                        isPlaying={isPlaying}
+                        activeSong={activeSong}
+                        data={data}
                         i={i}
                         />
                     ))
