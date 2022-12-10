d5365903690b2cfcacfa40b50b92d109fcc124a0
shubham-sanvariya <shubhamsanvariyait.com>
2022-12-10 22:06:00 +0530

    PlayPause updated

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -3,17 +3,9 @@ import { FaPauseCircle ,FaPlayCircle } from 'react-icons/fa'
 const PlayPause = ({ isPlaying, activeSong, song, 
 handlePause, handlePlay}) => (isPlaying && activeSong?.
 title === song.title ? (
-  <FaPauseCircle 
-    size={35}
-    className='text-gray-300'
-    onClick={handlePlay}
-  />
+        <button onClick={handlePause}><FaPauseCircle size={35} className="text-gray-300" /></button>
 ) : (
-  <FaPlayCircle 
-    size={35}
-    className='text-gray-300'
-    onClick={handlePause}
-  />
+        <button onClick={handlePlay}><FaPlayCircle size={35} className="text-gray-300" /></button>
 )
 
 );
