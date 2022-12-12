5cf770b8526b1a009d3e11b1c01052d887e90c6a
shubham-sanvariya <shubhamsanvariyait.com>
2022-12-12 11:38:42 +0530

    getSondDetails

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -12,8 +12,13 @@ export  const  shazamCoreApi  = createApi({
         },
     }),
     endpoints: (builder) => ({
-        getTopCharts: builder.query({ query: () => '/charts/world'}),
+        getTopCharts: builder.query({ query: () => '/charts/world'
+        }),
+        getSongDetails: builder.query({
+            query: ({ songid }) => 
+            `/tracks/details?track_id=${songid}`
+        }),
     }),
 });
 
-export const  {useGetTopChartsQuery}  =  shazamCoreApi
+export const  {useGetTopChartsQuery,useGetSongDetailsQuery}  =  shazamCoreApi
