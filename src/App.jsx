d20f30aab5eaaf50d14324b151f414ad1b121983
shubham-sanvariya <shubhamsanvariyait.com>
2022-12-12 11:38:21 +0530

    adding SongDetails component

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,7 @@ const App = () => {
           </div>
           <div className="xl:sticky relative top-0 h-fit">
             <TopPlay />
+            <SongDetails/>
           </div>
         </div>
       </div>
